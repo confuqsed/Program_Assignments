aaedf285882921e92ea78a20d617ad7e7ef0148d
confuqsed <cordenet@gmail.com>
2024-07-11 00:28:46 -0700

    fixed the hero stuff

--- Program_Assignments/SuperHeroBattleSim/hero.py
+++ Program_Assignments/SuperHeroBattleSim/hero.py
@@ -6,24 +6,46 @@ from armor import Armor
 class Hero:
     # We want our hero to have a default "starting_health",
     # so we can set that in the function header.
-    def __init__(self, name, starting_health=100):
-        '''Instance properties:
-        name: String
-        starting_health: Integer
+    # We know the name of our hero, so we assign it here
+    # Similarly, our starting health is passed in, just like name
+    # When a hero is created, their current health is
+    # Always the same as their starting health (no damage taken yet!)
+  
+    
 
-        current_health: Integer
-    '''
-        
-        # We know the name of our hero, so we assign it here
+    def __init__(self, name, starting_health=100):
+       
         self.name = name
-        # Similarly, our starting health is passed in, just like name
         self.starting_health = starting_health
-        # When a hero is created, their current health is
-        # Always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
         self.abilities = list()
         self.armors = list()
-        add_ability = ()
+
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+        
+    def defend(self):
+        total_damage = 0
+        for armor in self.armors:
+            total_damage -= armor.defend()
+        return total_damage
+        
+    def battle(self, opponent):
+        if self.abilities == []:
+            print("Draw")
+        else:
+            while self.current_health > 0 and opponent.current_health > 0:
+                opponent.take_damage(self.attack())
+                if opponent.current_health <= 0:
+                    print(f"{self.name} won!")
+                    return
+
+
+        
+
 
 if __name__ == "__main__":
     # If you run this file from the terminal
